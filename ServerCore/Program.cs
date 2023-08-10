2213135b846111f23f76eac923d6a23df901360a
githubbot7777 <shawncan1573@gmail.com>
2023-08-10 16:15:44 +0900

    test ignore

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,7 +6,7 @@ namespace ServerCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
         }
     }
 }
