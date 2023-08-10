0997c9e0ae6f63cd1553be59d5f45de47bfc8168
githubbot7777 <shawncan1573@gmail.com>
2023-08-10 16:30:26 +0900

    Socket 통신으로 메시지 전송

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
@@ -6,7 +9,38 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Dns(Domain in Name System)
+            string host = Dns.GetHostName();//로컬 컴퓨터의 호스트 이름을 가져옵니다.
+            IPHostEntry ipHost = Dns.GetHostEntry(host);//IPHostEntry 클래스 별칭의 배열 및 IP 주소와 일치 하는 배열을 사용 하 여 도메인 이름 시스템 (DNS) 호스트 이름을 연결 합니다.
+            IPAddress ipAddr = ipHost.AddressList[0];//호스트와 연결된 IP 주소 목록을 가져오거나 설정합니다.
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);//네트워크 엔드포인트를 IP 주소와 포트 번호로 나타냅니다.
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");//RemoteEndPoint 연결된 원격 IP 주소 및 포트 번호를 Socket 포함하는 값을 가져옵니다
+
+                //보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello world!");
+                int sendBytes = socket.Send(sendBuff);
+
+                //받는다
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server]{recvData}");
+
+                //나간다
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
         }
     }
 }
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
@@ -6,7 +9,49 @@ namespace ServerCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            //Dns(Domain in Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listenSocket.Bind(endPoint);//Socket을 로컬 엔드포인트와 연결합니다.
+                listenSocket.Listen(10); //backlog: 최대 대기수
+
+                while (true)
+                {
+                    Console.WriteLine("Listening....");
+
+                    //입장시킴
+                    Socket clientSocket = listenSocket.Accept();//blocking 함수, 클라이언트가 오지 않으면 여기서 멈춤
+
+                    //받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+                    //보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to server!");
+                    clientSocket.Send(sendBuff);//blocking 함수
+
+
+                    //쫓아낸다
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
         }
     }
 }
