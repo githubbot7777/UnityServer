d55b9828026d06d17d341112b9f03a35b832c3bd
githubbot7777 <shawncan1573@gmail.com>
2023-09-06 16:28:06 +0900

    Packet 관련코드 자동화

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,37 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using ServerCore;
 
 namespace Server
 {
-   
-    class Program
-    {
-        static Listener _listener = new Listener();
-
-        static void Main(string[] args)
-        {
-            //Dns(Domain in Name System)
-            string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
 
-            _listener.Init(endPoint, () => { return new ClientSession(); });
-            Console.WriteLine("Listening....");
+	class Program
+	{
+		static Listener _listener = new Listener();
 
-            while (true)
-            {
-                ;
-            }
+		static void Main(string[] args)
+		{
+			PacketManager.Instance.Register();
 
+			// DNS (Domain Name System)
+			string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+			_listener.Init(endPoint, () => { return new ClientSession(); });
+			Console.WriteLine("Listening...");
 
-
-        }
-    }
+			while (true)
+			{
+				;
+			}
+		}
+	}
 }
