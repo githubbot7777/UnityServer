3b4650ab4ead9c00259768bf33fc36bdef76d6f0
githubbot7777 <shawncan1573@gmail.com>
2023-09-14 23:46:55 +0900

    Server/JobTimer Timer 중앙관리

---
+++ Server/JobTimer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ServerCore;
+namespace Server
+{
+    struct JobTimerElem : IComparable<JobTimerElem>
+    {
+        public int execTick;//실행 시간
+        public Action action;
+
+        public int CompareTo(JobTimerElem other)
+        {
+            return other.execTick - execTick;
+        }
+    }
+    class JobTimer
+    {
+        PriorityQueue<JobTimerElem> _pq = new PriorityQueue<JobTimerElem>();//오래된 작업이 우선순위 높음
+        object _lock = new object();
+
+        public static JobTimer Instance { get; } = new JobTimer();
+
+        public void Push(Action action, int tickAfter=0)//tickAfter 몇초후에 실행할건지
+        {
+            JobTimerElem job;
+            job.execTick = System.Environment.TickCount + tickAfter;
+            job.action = action;
+
+            lock(_lock)
+            {
+                _pq.Push(job);
+            }
+        }
+
+        public void Flush()
+        {
+            while(true)
+            {
+                int now = System.Environment.TickCount;
+
+                JobTimerElem job;
+                lock(_lock)
+                {
+                    if (_pq.Count == 0)
+                        break;
+
+                    job = _pq.Peek();
+                    if (job.execTick > now)
+                        break;
+
+                    _pq.Pop();
+                    
+                }
+
+                job.action.Invoke();
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -15,6 +15,12 @@ namespace Server
 	{
 		static Listener _listener = new Listener();
 		public static GameRoom Room = new GameRoom();
+
+		static void FlushRoom()
+        {
+			Room.Push(() => Room.Flush());
+			JobTimer.Instance.Push(FlushRoom, 250);
+		}
 		static void Main(string[] args)
 		{
 			
@@ -27,11 +33,11 @@ namespace Server
 			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
 			Console.WriteLine("Listening...");
 
+			//FlushRoom();
+			JobTimer.Instance.Push(FlushRoom);
 			while (true)
 			{
-				Room.Push(() => Room.Flush());
-				Thread.Sleep(250);
-				;
+				JobTimer.Instance.Flush();
 			}
 		}
 	}
